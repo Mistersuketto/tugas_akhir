cf2df9cff55983e461b02abbeaae39f00fded39a
Mistersuketto <mistersuketto.153@gmail.com>
2025-07-12 01:15:59 +0900

    memindahkan file hsv.py dari yolov8 ke folder hsv

--- program/detection/hsv/hsv.py
+++ program/detection/hsv/hsv.py
@@ -1,21 +1,55 @@
 import cv2
+import numpy as np
+import cv2
+from PIL import Image
 
-cam = cv2.VideoCapture(0)
+
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+
+yellow = [1, 177, 208]  # yellow in BGR colorspace
+cap = cv2.VideoCapture(0)
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        break
+    ret, frame = cap.read()
 
-    # Convert the frame to HSV color space
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Display the original and HSV frames
-    cv2.imshow('Original Frame', frame)
-    cv2.imshow('HSV Frame', hsv_frame)
+    lowerLimit, upperLimit = get_limits(color=yellow)
 
-    # Exit on 'q' key press
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-# Release the camera and close all OpenCV windows
-cam.release()
+
+cap.release()
+
 cv2.destroyAllWindows()
